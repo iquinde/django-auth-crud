d392f336751d713692af916c8cd12f91ac7456c6
Israel Quinde <iquinde@salicadelecuador.com.ec>
2023-09-15 21:16:37 -0500

    first commit

---
+++ myapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Tasks
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ['title','description','important']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Write a title'}),
+            'description':forms.Textarea(attrs={'class':'form-control','placeholder':'Write a descrption'}),
+            'important':forms.CheckboxInput(attrs={'class':'form-check-input m-auto'})
+        }
